8d3c5037a9358cae943ba55c42c6070772308426
RownakDip <52921729+RownakDip@users.noreply.github.com>
2021-08-21 10:15:20 +0600

    manage,create,edit pages created

---
+++ tms/resources/views/backend/pages/tasklist/create.blade.php
@@ -0,0 +1,6 @@
+@extends('backend.layout.template')
+@section('body')
+
+<h1>USERS</h1>
+
+@endsection
---
+++ tms/resources/views/backend/pages/tasklist/manage.blade.php
@@ -0,0 +1,6 @@
+@extends('backend.layout.template')
+@section('body')
+
+<h1>ORDER</h1>
+
+@endsection
