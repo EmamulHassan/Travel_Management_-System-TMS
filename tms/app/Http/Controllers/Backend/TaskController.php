31a68ed3105a168ce051f131cf5d17a6d7ef8da5
RownakDip <52921729+RownakDip@users.noreply.github.com>
2021-08-21 10:48:33 +0600

    created 'order' model and migrated

--- tms/app/Http/Controllers/Backend/TaskController.php
+++ tms/app/Http/Controllers/Backend/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\order;
 
 class TaskController extends Controller
 {
--- tms/resources/views/backend/pages/tasklist/create.blade.php
+++ tms/resources/views/backend/pages/tasklist/create.blade.php
@@ -1,6 +1,68 @@
 @extends('backend.layout.template')
 @section('body')
 
-<h1>USERS</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="Rownak Islam">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Checkout</title>
+  <link rel='stylesheet' href='body.css' />
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css' />
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css' />
+</head>
+
+<body>
+
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-6 px-4 pb-4" id="order">
+        <h4 class="text-center text-info p-2">Complete your order!</h4>
+        <div class="jumbotron p-3 mb-2 text-center">
+          <h6 class="lead"><b>Destination(s) : </b></h6>
+          
+          <h5><b>Total Amount Payable : </b></h5>
+        </div>
+        <form action="" method="post" id="placeOrder">
+          <input type="hidden" name="products" value="">
+          <input type="hidden" name="grand_total" value="">
+          <div class="form-group">
+            <input type="text" name="name" class="form-control" placeholder="Enter Name" required>
+          </div>
+          <div class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Enter E-Mail" required>
+          </div>
+          <div class="form-group">
+            <input type="tel" name="phone" class="form-control" placeholder="Enter Phone" required>
+          </div>
+          <div class="form-group">
+            <textarea name="address" class="form-control" rows="3" cols="10" placeholder="Enter Your Address Here..."></textarea>
+          </div>
+          <h6 class="text-center lead">Select Payment Mode</h6>
+          <div class="form-group">
+            <select name="pmode" class="form-control">
+              <option value="" selected disabled>-Select Payment Mode-</option>
+              <option value="cod">Cash On Delivery</option>
+              <option value="netbanking">Net Banking</option>
+              <option value="cards">Debit/Credit Card</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <input type="submit" name="submit" value="Place Order" class="btn btn-danger btn-block">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js'></script>
+  </body>
+</html>
 
 @endsection
