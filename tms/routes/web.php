7df3fa8d5d5cb5052d4ca463e29f6bafecabbf2c
RownakDip <52921729+RownakDip@users.noreply.github.com>
2021-08-21 07:05:26 +0600

    dashboard removed.
    Will pull already existing dashboard

--- tms/routes/web.php
+++ tms/routes/web.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('backend.dashboard');
-});
+
 Route::get('/order', function () {
     return view('backend.order');
 });
