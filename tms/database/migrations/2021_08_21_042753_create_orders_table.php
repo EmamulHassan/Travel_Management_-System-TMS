8105a9a44e558a5f73e63eed14d2ef799a4d527c
RownakDip <52921729+RownakDip@users.noreply.github.com>
2021-08-28 07:49:43 +0600

    created order Model and Migration

---
+++ tms/database/migrations/2021_08_21_042753_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Name');
+            $table->string('E-mail');
+            $table->string('Phone');
+            $table->string('Address');
+            $table->string('PaymentMethod');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
