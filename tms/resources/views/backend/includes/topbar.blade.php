b36a9b6852cc34cd10f91403170ac08eb1e59237
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-09-01 22:13:11 +0600

    finished Admin Login and auth
    finished Admin Login and authentication.

--- tms/resources/views/backend/includes/topbar.blade.php
+++ tms/resources/views/backend/includes/topbar.blade.php
@@ -165,7 +165,12 @@
                 <li><a href=""><i class="icon ion-ios-download"></i> Downloads</a></li>
                 <li><a href=""><i class="icon ion-ios-star"></i> Favorites</a></li>
                 <li><a href=""><i class="icon ion-ios-folder"></i> Collections</a></li>
-                <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+                <li>
+                  <form method="POST" action="{{ route('logout') }}">
+                  @csrf
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();"><i class="icon ion-power"></i> Sign Out</a>
+                  </form>
+                </li>
               </ul>
             </div><!-- dropdown-menu -->
           </div><!-- dropdown -->
