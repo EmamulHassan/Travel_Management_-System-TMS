0e20e411d3e44c4730d46e4fff9068cc99779a43
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-05 05:18:30 +0600

    Update branch tested
    Update branch tested

--- tms/tests/Feature/Http/Controllers/Backend/BranchControllerTest.php
+++ tms/tests/Feature/Http/Controllers/Backend/BranchControllerTest.php
@@ -22,7 +22,7 @@ class BranchControllerTest extends TestCase
     {
         $User = User::factory()->create();
         $this->actingAs($User);
-        $response = $this->post('/admin/branch/storeee',[
+        $response = $this->post('/admin/branch/store',[
         'name' => 'Dhaka Branch',
         'bangla_name' =>'ঢাকা  ব্রাঞ্চ',
         'address_line1' => 'uttara',
@@ -30,6 +30,21 @@ class BranchControllerTest extends TestCase
         'email' =>'dhaka@tms.com',
         'phone' =>'01712345678',
         'status' =>'1',
+        ]);
+        $response->assertStatus(200);
+    }
+    public function branch_can_be_updated_through_form()
+    {
+        $User = User::factory()->create();
+        $this->actingAs($User);
+        $response = $this->post('/admin/branch/update/{1}',[
+        'name' => 'Dhaka Branch',
+        'bangla_name' =>'ঢাকা  ব্রাঞ্চ',
+        'address_line1' => 'uttara',
+        'address_line2' => 'dhaka',
+        'email' =>'dhaka@tms.com',
+        'phone' =>'01712345678',
+        'status' =>'2',
         ]);
         $response->assertStatus(200);
     }
