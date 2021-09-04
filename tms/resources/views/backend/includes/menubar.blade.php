6e5be67c134c19abbb809ddd6bc03bcef1c9e056
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-04 18:08:02 +0600

    Menu bar updated
    Menu bar updated  for managing orders

--- tms/resources/views/backend/includes/menubar.blade.php
+++ tms/resources/views/backend/includes/menubar.blade.php
@@ -20,7 +20,16 @@
           <ul class="br-menu-sub">
             <li class="sub-item"><a href="{{route('branch.manage')}}" class="sub-link">Manage All Branch</a></li>
             <li class="sub-item"><a href="{{route('branch.create')}}" class="sub-link">Create New Branch</a></li>
+          </ul>
+        </li>
 
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Orders</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="{{route('branch.manage')}}" class="sub-link">Manage All Orders</a></li>
           </ul>
         </li>
         {{-- <li class="br-menu-item">
