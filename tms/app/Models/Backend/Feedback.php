1460d9de6f49c51e90829494813c2e4b456ea366
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-09-04 20:53:39 +0600

    feedback model updated
    feedback model updated

--- tms/app/Models/Backend/Feedback.php
+++ tms/app/Models/Backend/Feedback.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'orderid',
+        'service',
+        'feedback',
+    ];
+    
 }
